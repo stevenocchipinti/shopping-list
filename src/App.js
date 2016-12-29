b37fe51fb90869cddd911e876e809b2c92cb2208
Steve Occhipinti <dev@stevenocchipinti.com>
2016-12-29 16:28:17 +1100

    Read data from Firebase POC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import * as firebase from "firebase";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { message: "Hello World" }
+  }
+
+  componentDidMount() {
+    firebase.database().ref("/message").on("value", snapshot => {
+      this.setState({
+        message: snapshot.val()
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +25,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.state.message }
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from "firebase";
 import App from './App';
 import './index.css';
+
+const config = {
+  apiKey: "AIzaSyCzW5_IINwJFTEM0VqSyYCRgNGlzYLoSno",
+  authDomain: "fatlog-staging.firebaseapp.com",
+  databaseURL: "https://fatlog-staging.firebaseio.com",
+  storageBucket: "fatlog-staging.appspot.com",
+  messagingSenderId: "1031126490652"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
