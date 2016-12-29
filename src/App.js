0cb828b1eb81f8b9ba1aaa6ade094367480ac2be
Steve Occhipinti <dev@stevenocchipinti.com>
2016-12-30 10:29:56 +1100

    Basic material UI - needs refactoring!

--- src/App.js
+++ src/App.js
@@ -1,43 +1,127 @@
 import React, { Component } from 'react';
 import * as firebase from "firebase";
+
+import AppBar from 'material-ui/AppBar';
+import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
+import { TableHeader, TableHeaderColumn } from 'material-ui/Table';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import RefreshIndicator from 'material-ui/RefreshIndicator';
+
 import NewEntryForm from './NewEntryForm';
-import logo from './logo.svg';
-import './App.css'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      messages: []
+      checkins: []
     }
   }
 
   componentDidMount() {
-    firebase.database().ref("/messages").on("value", snapshot => {
-      const messages = snapshot.val();
+    firebase.database().ref("/checkins").on("value", snapshot => {
+      const checkins = snapshot.val();
       this.setState({
-        messages: Object.keys(messages).map(k => {
-          return { key: k, message: messages[k].message }
+        checkins: Object.keys(checkins).reverse().map(k => {
+          return {
+            key: k,
+            date: checkins[k].createdAt,
+            weight: checkins[k].weight,
+            fat: checkins[k].fat,
+            waist: checkins[k].waist
+          }
         })
       })
     });
   }
 
   render() {
-    const messageComponents = this.state.messages.map(message => {
-      return <li key={message.key}>{message.message}</li>
+    const tableCellStyle = {
+      padding: "10px",
+      textAlign: "center",
+      textOverflow: "none"
+    };
+    const tableMenuCellStyle = {
+      padding: "0px",
+      width: "30px"
+    }
+
+    const iconButton = (
+      <IconButton style={tableMenuCellStyle}><MoreVertIcon /></IconButton>
+    );
+
+    const LoadingSpinner = (
+      <div style={{ marginTop: "30px", textAlign: "center", position: "relative" }}>
+        <RefreshIndicator
+          size={40}
+          left={10}
+          top={0}
+          status="loading"
+          style={{ display: "inline-block", position: "relative" }}
+        />
+      </div>
+    );
+
+
+    const rows = this.state.checkins.map(checkin => {
+      const date = new Date(checkin.date);
+      return (
+        <TableRow key={ checkin.key }>
+          <TableRowColumn style={ tableCellStyle }>
+            { `${date.getDate()}/${date.getMonth()}` }
+            <br />
+            { date.getFullYear() }
+          </TableRowColumn>
+          <TableRowColumn style={ tableCellStyle }>
+            { checkin.weight } kg
+          </TableRowColumn>
+          <TableRowColumn style={ tableCellStyle }>
+            { checkin.fat } %
+          </TableRowColumn>
+          <TableRowColumn style={ tableCellStyle }>
+            { checkin.waist } cm
+          </TableRowColumn>
+          <TableRowColumn style={ tableMenuCellStyle }>
+            <IconMenu
+              iconButtonElement={iconButton}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="Edit" />
+              <MenuItem primaryText="Delete" />
+            </IconMenu>
+          </TableRowColumn>
+        </TableRow>
+      );
     });
+
+    const table = (
+      <Table selectable={false}>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn style={ tableCellStyle }>Date</TableHeaderColumn>
+            <TableHeaderColumn style={ tableCellStyle }>Weight</TableHeaderColumn>
+            <TableHeaderColumn style={ tableCellStyle }>Fat</TableHeaderColumn>
+            <TableHeaderColumn style={ tableCellStyle }>Waist</TableHeaderColumn>
+            <TableHeaderColumn style={ tableMenuCellStyle }></TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          { rows }
+        </TableBody>
+      </Table>
+    );
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <ul>{ messageComponents }</ul>
+        <AppBar title="FatLog" showMenuIconButton={false} />
         <NewEntryForm />
+        { (this.state.checkins.length === 0) ? LoadingSpinner : table }
       </div>
     );
   }
 }
-
 export default App;
--- src/NewEntryForm.js
+++ src/NewEntryForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
 
 export default props => {
   return (
-    <section>
-      <RaisedButton>hello</RaisedButton>
-    </section>
+    <Paper zDepth={1} style={{ overflow: "hidden", margin: "20px", padding: "20px" }}>
+      <DatePicker fullWidth={true} floatingLabelText="Date" defaultDate={new Date()} />
+      <TextField fullWidth={true} floatingLabelText="Middle name" />
+      <TextField fullWidth={true} floatingLabelText="Last name" />
+      <TextField fullWidth={true} floatingLabelText="Email address" />
+      <RaisedButton label="Submit" primary={true} style={{ marginTop: "20px", float: "right" }} />
+    </Paper>
   );
 }
