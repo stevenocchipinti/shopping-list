a9a9386f4f2d21b34d5c890dc0611a40dc0b10a1
Steve Occhipinti <dev@stevenocchipinti.com>
2017-11-10 01:25:31 +1100

    Build a homepage component
    New hero image:
      URL: https://unsplash.com/photos/-gOUx23DNks
      Photo by ja ma on Unsplash

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from "react";
+import background from './../assets/images/background.jpg';
+
+export default class App extends PureComponent {
+  render() {
+    const styles = {
+      container: {
+        background: `
+          linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.8)),
+          url(${background}) no-repeat 50% fixed
+        `,
+        backgroundSize: "cover",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: "100vh"
+      },
+      heading: {
+        color: "white",
+        fontSize: "75px",
+        textAlign: "center",
+        lineHeight: 1.5
+      },
+      cta: {
+        color: "white",
+        textDecoration: "none",
+        border: "3px solid white",
+        borderRadius: 10,
+        fontSize: "30px",
+        padding: 30
+      }
+    }
+
+    return (
+      <main style={styles.container}>
+        <h1 style={styles.heading}>Shopping List</h1>
+        <a href="/list/demo" style={styles.cta}>Try the demo</a>
+      </main>
+    );
+  }
+}
