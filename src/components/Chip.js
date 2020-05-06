f730b3eba95eea6049cdbe44f095e5c2dfb6c045
Steve Occhipinti <dev@stevenocchipinti.com>
2020-05-06 20:55:40 +1000

    Visual tweak to the quantity
    The SVG version of the 'x' was easier to position in the center rather than
    using the text character which inherently has some space above/below it.

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -8,13 +8,15 @@ const greys = (light, dark) => ({ theme }) =>
     : theme.palette.grey[dark]
 
 const Chip = styled.span`
+  display: flex;
+  align-items: center;
   background-color: ${greys("300", "900")};
   font-size: 14px;
   line-height: 32px;
   border-radius: 16px;
   padding: 0 12px;
   margin: 0.25rem;
-  color: ${({ done }) => (done ? "#aaa" : "inherit")};
+  color: ${({ done, theme }) => (done ? theme.palette.grey.A200 : "inherit")};
 `
 
 const Value = styled.span`
@@ -22,24 +24,34 @@ const Value = styled.span`
 `
 
 const Qty = styled.span`
+  display: inline-flex;
+  align-items: center;
   line-height: 16px;
   background-color: ${greys("200", "800")};
   border: 1px solid ${greys("400", "900")};
   border-radius: 0 18px 18px 0;
-  padding: 4px 8px 4px 6px;
-  margin: 0 -8px 0 8px;
+  padding: 4px 7px 4px 6px;
+  margin: 0 -9px 0 7px;
 `
 
-const X = styled.span`
-  font-size: 10px;
+const Svg = styled.svg`
+  stroke: ${greys("A200", "500")};
+  margin-right: 3px;
 `
+
+const X = () => (
+  <Svg height={6} width={6}>
+    <path d="M0 0 L 6 6" />
+    <path d="M6 0 L 0 6" />
+  </Svg>
+)
 
 export default ({ done, qty, children, ...props }) => (
   <Chip done={done} {...props}>
     <Value done={done}>{children}</Value>
     {qty && qty > 1 && (
       <Qty>
-        <X>&#x2715;</X>
+        <X />
         {qty}
       </Qty>
     )}
